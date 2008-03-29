65ff13e1f844e6cf5a95ed6ef1914db2d2460efc
feu <feu@f4845f57-1051-4a60-b652-85e1510f23c6>
2008-03-29 02:28:08 +0000

    LoggerHead: - UTF-8 format is better - Add LoggerHeadLocale-frFR.lua to .toc file

--- tmp/trunk/LoggerHead/LoggerHeadLocale-frFR.lua
+++ tmp/trunk/LoggerHead/LoggerHeadLocale-frFR.lua
@@ -1,22 +1,22 @@
-local L = AceLibrary("AceLocale-2.2"):new("LoggerHead")
+ï»¿local L = AceLibrary("AceLocale-2.2"):new("LoggerHead")
 
 L:RegisterTranslations("frFR", function() return {
-	["Toggle Logging"] = "Activer/Désactiver les logs",
+	["Toggle Logging"] = "Activer/DÃ©sactiver les logs",
 	["Instances"] = "Instances",
-	["Instance log settings"] = "Paramètres de log d'instance",
+	["Instance log settings"] = "ParamÃ¨tres de log d'instance",
 	["Zones"] = "Zones",
-	["Zone log settings"] = "Paramètres de log de zone",    
+	["Zone log settings"] = "ParamÃ¨tres de log de zone",    
 	["Eastern Kingdoms"] = "Royaumes de l'est",
 	["Kalimdor"] = "Kalimdor",
 	["Outland"] = "Outreterre",
-	["You have entered %s. Do you want to enable logging for this zone/instance?"] = "Vous êtes entré dans %s. Voulez-vous activer les logs pour cette zone/instance ?",
+	["You have entered %s. Do you want to enable logging for this zone/instance?"] = "Vous Ãªtes entrÃ© dans %s. Voulez-vous activer les logs pour cette zone/instance ?",
 	["Enable"] = "Activer",
-	["Disable"] = "Désactiver",
-	["Enabled"] = "|cff00ff00Activé|r",
-	["Disabled"] = "|cffff0000Désactivé|r",
+	["Disable"] = "DÃ©sactiver",
+	["Enabled"] = "|cff00ff00ActivÃ©|r",
+	["Disabled"] = "|cffff0000DÃ©sactivÃ©|r",
 	["Combat Log"] = "Log de combat",
 	["Shift-Click to open configuration"] = "Maj-Clic pour ouvrir la configuration",
-	["Click to toggle combat logging"] = "Clic pour activer/désactiver le log de combat",
+	["Click to toggle combat logging"] = "Clic pour activer/dÃ©sactiver le log de combat",
 	["Prompt on new zone?"] = "Demander dans une nouvelle zone ?",
 	["Prompt when entering a new zone?"] = "Demander en entrant dans une nouvelle zone ?",
 } end)
