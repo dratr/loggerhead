ce7b0796653ba2a182605f60ac9a89ef4dc75100
sano <sano@f4845f57-1051-4a60-b652-85e1510f23c6>
2008-07-24 17:17:48 +0000

    LoggerHead: Moved 2.0 to trunk.  Add LDB support and uses Ace3

---
+++ tmp/trunk/LoggerHead/LoggerHeadLocale-enUS.lua
@@ -0,0 +1,24 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("LoggerHead", "enUS", true)
+
+
+L["LoggerHead"] = true
+L["Toggle Logging"] = true
+L["Instances"] = true
+L["Instance log settings"] = true
+L["Zones"] = true
+L["Zone log settings"] = true    
+L["Eastern Kingdoms"] = true
+L["Kalimdor"] = true
+L["Outland"] = true
+L["You have entered |cffd9d919%s|r. Do you want to enable logging for this zone/instance?"] = true
+L["Enable"] = true
+L["Disable"] = true
+L["Enabled"] = "|cff00ff00Enabled|r"
+L["Disabled"] = "|cffff0000Disabled|r"
+L["Combat Log"] = true
+L["Shift-Click to open configuration"] = true
+L["Click to toggle combat logging"] = true
+L["Prompt on new zone?"] = true
+L["Prompt when entering a new zone?"] = true
+L["Combat Log Enabled"] = true
+L["Combat Log Disabled"] = true
