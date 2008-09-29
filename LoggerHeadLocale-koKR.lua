9ff4cd5f4de791d31c60de2c5c78d6378fededd9
root <root@f4845f57-1051-4a60-b652-85e1510f23c6>
2008-09-29 20:58:20 +0000

    Importing old repo data under /trunk

---
+++ LoggerHeadLocale-koKR.lua
@@ -0,0 +1,24 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("LoggerHead", "koKR")
+if not L then return end
+
+--L["LoggerHead"] = true
+L["Toggle Logging"] = "로그 파일 기록 토글"
+L["Instances"] = "인스턴스"
+L["Instance log settings"] = "인스턴스 로그 설정"
+L["Zones"] = "지역"
+L["Zone log settings"] = "지역 로그 설정"
+L["Eastern Kingdoms"] = "동부 왕국"
+L["Kalimdor"] = "칼림도어"
+L["Outland"] = "아웃랜드"
+L["You have entered |cffd9d919%s|r. Do you want to enable logging for this zone/instance?"] = "\"|cffd9d919%s.|r\"에 들어섰습니다. 이 지역(인스턴스던전)에 대한 로그를 파일로 기록하시겠습니까?"
+L["Enable"] = "가능"
+L["Disable"] = "불가능"
+L["Enabled"] = "|cff00ff00가능|r"
+L["Disabled"] = "|cffff0000불가능|r"
+L["Combat Log"] = "전투 로그"
+L["Shift-Click to open configuration"] = "쉬프트-클릭: 설정창 열기"
+L["Click to toggle combat logging"] = "클릭: 전투 로그 토글"
+L["Prompt on new zone?"] = "새로운 지역 바로 기록"
+L["Prompt when entering a new zone?"] = "새로운 지역에 들어서면 로그 기록을 바로 시작하시겠습니까?"
+--L["Combat Log Enabled"] = true
+--L["Combat Log Disabled"] = true
