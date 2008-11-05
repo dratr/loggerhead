1136c88a72075684c5b61dbf4946385aafccab51
rsmozang <rsmozang@f4845f57-1051-4a60-b652-85e1510f23c6>
2008-11-05 21:01:43 +0000

    Added preliminary Northrend config options Config now loads dynamically reducing memory footprint

--- LoggerHead.lua
+++ LoggerHead.lua
@@ -2,6 +2,7 @@ local LoggerHead = LibStub("AceAddon-3.0"):NewAddon("LoggerHead", "AceConsole-3.
 
 local L = LibStub("AceLocale-3.0"):GetLocale("LoggerHead", true)
 local T = LibStub("LibTourist-3.0")
+local BZ = LibStub("LibBabble-Zone-3.0"):GetLookupTable()
 
 LoggerHead.dd = LibStub("LibDataBroker-1.1"):NewDataObject("LoggerHead", {
 	icon = "Interface\\AddOns\\LoggerHead\\disabled", 
@@ -9,7 +10,7 @@ LoggerHead.dd = LibStub("LibDataBroker-1.1"):NewDataObject("LoggerHead", {
 	text = L["Disabled"],
 	OnClick = function(self, button)
 		if button == "RightButton" then
-			LibStub("AceConfigDialog-3.0"):Open("LoggerHead")
+			LoggerHead:LoadConfig()
 		end
 		
 		if button == "LeftButton" then
@@ -32,137 +33,137 @@ local defaults = {
 	}
 }
 
-local options = {
-	name = 'Loggerhead',
-	type = "group",
-	args = {
-		instances = {
-			order = 1,
-			type = "group",
-			name = L["Instances"],
-			desc = L["Instance log settings"],
-			args = {
-				easternkingdoms = {
-					type = "group",
-					name = L["Eastern Kingdoms"],
-					desc = L["Instance log settings"],
-					args = {},
-				},
-				kalimdor = {
-					type = "group",
-					name = L["Kalimdor"],
-					desc = L["Instance log settings"],
-					args = {},
-				},
-				outland = {
-					type = "group",
-					name = L["Outland"],
-					desc = L["Instance log settings"],
-					args = {},
-				},
-			},
-		},
-		zones = {
-			order = 2,
-			type = "group",
-			name = L["Zones"],
-			desc = L["Zone log settings"],
-			args = {
-				easternkingdoms = {
-					type = "group",
-					name = L["Eastern Kingdoms"],
-					desc = L["Zone log settings"],
-					args = {},
-				},
-				kalimdor = {
-					type = "group",
-					name = L["Kalimdor"],
-					desc = L["Zone log settings"],
-					args = {},
-				},
-				outland = {
-					type = "group",
-					name = L["Outland"],
-					desc = L["Zone log settings"],
-					args = {},
+local function createoptions()
+	local options = {
+		name = 'Loggerhead',
+		type = "group",
+		args = {
+			instances = {
+				order = 1,
+				type = "group",
+				name = L["Instances"],
+				desc = L["Instance log settings"],
+				args = {
+					EasternKingdoms = {
+						type = "group",
+						name = BZ["Eastern Kingdoms"],
+						desc = L["Instance log settings"],
+						args = {},
+					},
+					Kalimdor = {
+						type = "group",
+						name = BZ["Kalimdor"],
+						desc = L["Instance log settings"],
+						args = {},
+					},
+					Outland = {
+						type = "group",
+						name = BZ["Outland"],
+						desc = L["Instance log settings"],
+						args = {},
+					},
+					Northrend = {
+						type = "group",
+						name = BZ["Northrend"],
+						desc = L["Instance log settings"],
+						args = {},
+					},
 				},
 			},
+			zones = {
+				order = 2,
+				type = "group",
+				name = L["Zones"],
+				desc = L["Zone log settings"],
+				args = {
+					EasternKingdoms = {
+						type = "group",
+						name = BZ["Eastern Kingdoms"],
+						desc = L["Instance log settings"],
+						args = {},
+					},
+					Kalimdor = {
+						type = "group",
+						name = BZ["Kalimdor"],
+						desc = L["Instance log settings"],
+						args = {},
+					},
+					Outland = {
+						type = "group",
+						name = BZ["Outland"],
+						desc = L["Instance log settings"],
+						args = {},
+					},
+					Northrend = {
+						type = "group",
+						name = BZ["Northrend"],
+						desc = L["Instance log settings"],
+						args = {},
+					},
+				},
+			},
+			--spacer = { type = "header", order = 3 },
+			prompt = {
+				order = 5,
+				type = "toggle",
+				name = L["Prompt on new zone?"],
+				desc = L["Prompt when entering a new zone?"],
+				get = function() return LoggerHead.db.profile.prompt end,
+				set = function(v) LoggerHead.db.profile.prompt = not LoggerHead.db.profile.prompt end,
+			},
 		},
-		--spacer = { type = "header", order = 3 },
-		prompt = {
-			order = 5,
-			type = "toggle",
-			name = L["Prompt on new zone?"],
-			desc = L["Prompt when entering a new zone?"],
-			get = function() return LoggerHead.db.profile.prompt end,
-			set = function(v) LoggerHead.db.profile.prompt = not LoggerHead.db.profile.prompt end,
-		},
-	},
-}
+	}
 
+	for zone in T:IterateZonesAndInstances() do
+		local continent = (T:GetContinent(zone)):gsub(" ","")
+
+		if (continent ~= UNKNOWN) then
+			local type = T:IsInstance(zone) and "instances" or "zones"	
+			local key = zone:gsub(" ", "_")
+			options.args[type].args[continent].args[key] = {
+				type = "toggle",
+				name = zone,
+				desc = L["Toggle Logging"],
+				get = function() return LoggerHead.db.profile.log[zone] end,
+				set = function(v) LoggerHead.db.profile.log[zone] = not LoggerHead.db.profile.log[zone] end,
+			}
+		end
+	end
+
+	options.args.profile = LibStub("AceDBOptions-3.0"):GetOptionsTable(LoggerHead.db)
+	options.args.output = LoggerHead:GetSinkAce3OptionsDataTable()
+
+	return options
+end
+
+local options = nil
+local function loadoptions()
+	options = createoptions()
+
+	LibStub("AceConfig-3.0"):RegisterOptionsTable("LoggerHead", options)
+	LibStub("AceConfigDialog-3.0"):SetDefaultSize("LoggerHead", 800, 600)
+	local blizzPanel = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("LoggerHead", "LoggerHead")
+	LoggerHead:RegisterChatCommand("lh", LoggerHead.LoadConfig )
+end
+
+local hijackFrame = CreateFrame("Frame", nil, InterfaceOptionsFrame)
+hijackFrame:SetScript("OnShow", function(self)
+	if not options then
+		loadoptions()
+	end
+
+	self:SetScript("OnShow", nil)
+end)
+
+function LoggerHead:LoadConfig()
+	if not options then
+		loadoptions()
+	end
+
+	LibStub("AceConfigDialog-3.0"):Open("LoggerHead")
+end
 
 function LoggerHead:OnInitialize()
-	for zone in T:IterateEasternKingdoms() do
-		local key = zone:gsub(" ", "_")
-		if T:IsInstance(zone) then
-			options.args.instances.args.easternkingdoms.args[key] = {
-				type = "toggle",
-				name = zone,
-				desc = L["Toggle Logging"],
-				get = function() return LoggerHead.db.profile.log[zone] end,
-				set = function(v) LoggerHead.db.profile.log[zone] = not LoggerHead.db.profile.log[zone] end,
-			}
-		else
-				options.args.zones.args.easternkingdoms.args[key] = {
-				type = "toggle",
-				name = zone,
-				desc = L["Toggle Logging"],
-				get = function() return LoggerHead.db.profile.log[zone] end,
-				set = function(v) LoggerHead.db.profile.log[zone] = not LoggerHead.db.profile.log[zone] end,
-			}
-		end
-	end
-	for zone in T:IterateKalimdor() do
-		local key = zone:gsub(" ", "_")
-		if T:IsInstance(zone) then
-			options.args.instances.args.kalimdor.args[key] = {
-				type = "toggle",
-				name = zone,
-				desc = L["Toggle Logging"],
-				get = function() return LoggerHead.db.profile.log[zone] end,
-				set = function(v) LoggerHead.db.profile.log[zone] = not LoggerHead.db.profile.log[zone] end,
-			}
-		else
-				options.args.zones.args.kalimdor.args[key] = {
-				type = "toggle",
-				name = zone,
-				desc = L["Toggle Logging"],
-				get = function() return LoggerHead.db.profile.log[zone] end,
-				set = function(v) LoggerHead.db.profile.log[zone] = not LoggerHead.db.profile.log[zone] end,
-			}
-		end
-	end
-	for zone in T:IterateOutland() do
-		local key = zone:gsub(" ", "_")
-		if T:IsInstance(zone) then
-			options.args.instances.args.outland.args[key] = {
-				type = "toggle",
-				name = zone,
-				desc = L["Toggle Logging"],
-				get = function() return LoggerHead.db.profile.log[zone] end,
-				set = function(v) LoggerHead.db.profile.log[zone] = not LoggerHead.db.profile.log[zone] end,
-			}
-		else
-			options.args.zones.args.outland.args[key] = {
-				type = "toggle",
-				name = zone,
-				desc = L["Toggle Logging"],
-				get = function() return LoggerHead.db.profile.log[zone] end,
-				set = function(v) LoggerHead.db.profile.log[zone] = not LoggerHead.db.profile.log[zone] end,
-			}
-		end
-	end
-	
 	StaticPopupDialogs["LoggerHeadLogConfirm"] = {
 		text = L["You have entered |cffd9d919%s|r. Do you want to enable logging for this zone/instance?"],
 		button1 = L["Enable"],
@@ -183,16 +184,7 @@ function LoggerHead:OnInitialize()
 	
 	self.db = LibStub("AceDB-3.0"):New("LoggerHeadDB", defaults, "Default")
 	db = self.db.profile
-	
-	options.args.profile = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
-	
 	self:SetSinkStorage(self.db.profile.sink)
-	options.args.output = self:GetSinkAce3OptionsDataTable()
-
-	LibStub("AceConfig-3.0"):RegisterOptionsTable("LoggerHead", options)
-	LibStub("AceConfigDialog-3.0"):SetDefaultSize("LoggerHead", 800, 600)
-	LibStub("AceConfigDialog-3.0"):AddToBlizOptions("LoggerHead", "LoggerHead")
-	self:RegisterChatCommand("lh", function() LibStub("AceConfigDialog-3.0"):Open("LoggerHead") end)	
 end
 
 
