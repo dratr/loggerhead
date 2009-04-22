e522c5c1b3523d8709acbefa52097c044e23edcf
rsmozang <rsmozang@f4845f57-1051-4a60-b652-85e1510f23c6>
2009-04-22 22:06:52 +0000

    - Added support for area difficulty (Normal/Heroic) - reworked config panel - added LibDBIcon support - switched to the WowAce Localization tool - using updated TouristLib with Ulduar support - added AddonLoader support

--- LoggerHeadLocale-zhTW.lua
+++ LoggerHeadLocale-zhTW.lua
@@ -1,24 +1,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("LoggerHead", "zhTW")
 if not L then return end
 
---L["LoggerHead"] = true
-L["Toggle Logging"] = "開關日誌"
-L["Instances"] = "副本"
-L["Instance log settings"] = "設定副本日誌"
-L["Zones"] = "地區"
-L["Zone log settings"] = "設定地區日誌"
-L["Eastern Kingdoms"] = "東部王國"
-L["Kalimdor"] = "卡林多"
-L["Outland"] = "外域"
-L["You have entered |cffd9d919%s|r. Do you want to enable logging for this zone/instance?"] = "你已經進入 |cffd9d919%s.|r 您想要為這地區/副本啟動紀錄日誌嗎?"
-L["Enable"] = "啟動"
-L["Disable"] = "關閉"
-L["Enabled"] = "|cff00ff00記錄中|r"
-L["Disabled"] = "|cffff0000未記錄|r"
-L["Combat Log"] = "戰鬥日誌"
-L["Shift-Click to open configuration"] = "shift 點擊設置"
-L["Click to toggle combat logging"] = "點擊開啟/關閉記錄戰斗日志"
-L["Prompt on new zone?"] = "切換地區時詢問?"
-L["Prompt when entering a new zone?"] = "切換地區時詢問是否記錄戰斗日志?"
-L["Combat Log Enabled"] = "開始記錄戰斗日志"
-L["Combat Log Disabled"] = "停止記錄戰斗日志"
+--@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
