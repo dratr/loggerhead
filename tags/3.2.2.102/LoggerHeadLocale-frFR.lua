2677683a2ba486e727e7ff10ebeb415204a75fbc
rsmozang <rsmozang@f4845f57-1051-4a60-b652-85e1510f23c6>
2009-09-25 21:44:26 +0000

    Tagging as 3.2.2.102

---
+++ tags/3.2.2.102/LoggerHeadLocale-frFR.lua
@@ -0,0 +1,5 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("LoggerHead", "frFR")
+if not L then return end
+
+--@localization(locale="frFR", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
