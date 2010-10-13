8afccba4b2b9be5eb4f2b1231b8e678bf586b02d
rsmozang <rsmozang@f4845f57-1051-4a60-b652-85e1510f23c6>
2010-10-13 16:01:55 +0000

    Tagging as Release 4.0.117

---
+++ tags/Release 4.0.117/LoggerHeadLocale-enUS.lua
@@ -0,0 +1,9 @@
+local debug = false
+--@debug@
+debug = true
+--@end-debug@
+
+local L = LibStub("AceLocale-3.0"):NewLocale("LoggerHead", "enUS", true, debug)
+
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
