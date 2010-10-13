77d19930572aaf6d98b5e1b78cde837fac4c26fb
rsmozang <rsmozang@f4845f57-1051-4a60-b652-85e1510f23c6>
2010-10-13 01:33:45 +0000

    Tagging as Release 4.0.115

---
+++ tags/Release 4.0.115/LoggerHeadLocale-zhCN.lua
@@ -0,0 +1,4 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("LoggerHead", "zhCN")
+if not L then return end
+
+--@localization(locale="zhCN", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
