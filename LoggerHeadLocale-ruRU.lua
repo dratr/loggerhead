f3160f48299a96928814c05713925fd85e4aabfa
durcyn <durcyn@f4845f57-1051-4a60-b652-85e1510f23c6>
2012-12-03 08:14:38 +0000

    move locale files into subdirectory for no good reason

--- LoggerHeadLocale-ruRU.lua
+++
@@ -1,5 +0,0 @@
-﻿local ADDON_NAME, ADDON_TABLE = ...
-local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "ruRU")
-if not L then return end
-
---@localization(locale="ruRU", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
--- LoggerHeadLocale-zhTW.lua
+++
@@ -1,5 +0,0 @@
-local ADDON_NAME, ADDON_TABLE = ...
-local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "zhTW")
-if not L then return end
-
---@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
