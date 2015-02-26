a8963d381fd4fd8b8bcb030a4d639a1761187e31
dratr <dratr@f4845f57-1051-4a60-b652-85e1510f23c6>
2015-02-26 05:28:32 +0000

    Tagging as 6.1.0

---
+++ tags/6.1.0/Locale/LoggerHeadLocale-esMX.lua
@@ -0,0 +1,6 @@
+﻿local ADDON_NAME, ADDON_TABLE = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "esMX")
+if not L then return end
+
+--@localization(locale="esMX", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
