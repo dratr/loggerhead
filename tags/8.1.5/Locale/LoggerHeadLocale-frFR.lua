5c64132e33add213e1e96054ac942c2b422a34ac
dratr <dratr@f4845f57-1051-4a60-b652-85e1510f23c6>
2019-04-07 04:38:35 +0000

    Tag for release

---
+++ tags/8.1.5/Locale/LoggerHeadLocale-frFR.lua
@@ -0,0 +1,6 @@
+﻿local ADDON_NAME, ADDON_TABLE = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "frFR")
+if not L then return end
+
+--@localization(locale="frFR", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+
